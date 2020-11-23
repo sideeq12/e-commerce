184de25c322dc941055d7f9541730b2cb25ba20b
sideeq12 <abdwaheed2018@gmail.com>
2020-11-23 12:13:39 -0800

    process

--- src/components/wrappers/App.js
+++ src/components/wrappers/App.js
@@ -1,7 +1,46 @@
 import React, {Component} from 'react';
 
 function App(){
-    return(<h1>hello world!</h1>)
+    return(
+        <div className="menu-content">
+            <div className="box">
+                <div className="inner-box">
+                    <h2>hello</h2>
+                    <span>Message</span>
+                </div>
+            </div>
+            <div className="box">
+                <div className="inner-box">
+                    <h2>hello</h2>
+                    <span>Message</span>
+                </div>
+            </div>
+            <div className="box">
+                <div className="inner-box">
+                    <h2>hello</h2>
+                    <span>Message</span>
+                </div>
+            </div>
+            <div className="box">
+                <div className="inner-box">
+                    <h2>hello</h2>
+                    <span>Message</span>
+                </div>
+            </div>
+            <div className="box">
+                <div className="inner-box">
+                    <h2>hello</h2>
+                    <span>Message</span>
+                </div>
+            </div>
+            <div className="box">
+                <div className="inner-box">
+                    <h2>hello</h2>
+                    <span>Message</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
