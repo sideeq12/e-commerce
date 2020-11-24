34aedc31e48668ac4de1e6c766589b5541933a0e
sideeq12 <abdwaheed2018@gmail.com>
2020-11-24 05:11:41 -0800

    style

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -4,9 +4,10 @@ import "../menu-item/menu.css"
 function MenuItem({title, imageUrl}){ 
     return(
        
-<div className="box" style={{
+<div className="box">
+<div className="back"  style={{
     backgroundImage : `url(${imageUrl})`
-}}>
+}} ></div>
                 <div className="inner-box">
                     <h2>{title}</h2>
                     <span>SHOP NOW</span>
