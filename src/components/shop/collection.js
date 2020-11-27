fb3ae2e83bea3e4bfbb2b01fdb6f8271b9674bbf
sideeq12 <abdwaheed2018@gmail.com>
2020-11-27 00:58:51 -0800

    collections

--- src/components/shop/collection.js
+++ src/components/shop/collection.js
@@ -9,6 +9,21 @@ const collections = [
                 name : "BRIXTON'S MENS HATS",
                 imageUrl : "https://www.brixton.com/pub/media/catalog/category/HO19-GIFT-GUIDE-CLP-HAT-SHOP.jpg",
                 price : "$56"
+            }, {
+                id : 2,
+                name : "HEIST BEANIE - MUSTARRD",
+                imageUrl : "https://smhttp-ssl-77968-media.nexcesscdn.net/pub/media/catalog/product/cache/742e39a3389b1e0434b3c1c292844b69/h/e/heist-beanie_10782_mustr_001.jpg",
+                price : "$19"
+            }, {
+                id : 3,
+                name : "MELTER BUCKET HAT",
+                imageUrl : "https://smhttp-ssl-77968-media.nexcesscdn.net/pub/media/catalog/product/cache/742e39a3389b1e0434b3c1c292844b69/m/e/melter-bucket-hat_10611_black_01.jpg",
+                price : "$27"
+            },{
+                id : 4,
+                name : "WESLEY FEDORA HEALTHY/BLACK",
+                imageUrl : "https://smhttp-ssl-77968-media.nexcesscdn.net/pub/media/catalog/product/cache/742e39a3389b1e0434b3c1c292844b69/w/e/wesley-fedora_00212_htblk_01_61.jpg",
+                price : "$14"
             }
         ]
     },{
@@ -18,9 +33,24 @@ const collections = [
         content : [
             {
                 id : 1,
-                name : "",
-                imageUrl : "",
+                name : "EYE BOGLER",
+                imageUrl : "https://m.media-amazon.com/images/I/61xOjMsr6HL._AC_UL320_.jpg",
                 price : "$56"
+            }, {
+                id : 2, 
+                name : "ZAKOD",
+                imageUrl : "https://m.media-amazon.com/images/I/71tu2huhepL._AC_UL320_.jpg",
+                price : "$112"
+            },{
+                id : 3,
+                name : "KRISHNA EMPORIA",
+                imageUrl : "https://m.media-amazon.com/images/I/41tmo4f0I3L._AC_UL320_.jpg",
+                price : "$421"
+            }, {
+                id : 4,
+                name : "GENERIC",
+                imageUrl : "https://m.media-amazon.com/images/I/61lx5LijLCL._AC_UL320_.jpg",
+                price : "$212"
             }
         ] 
     },{
@@ -30,9 +60,24 @@ const collections = [
     content : [
         {
             id : 1,
-            name : "",
-            imageUrl : "",
+            name : "NIKE ADAPT BB",
+            imageUrl : "https://image.cnbcfm.com/api/v1/image/105680013-1547583426762nike1.jpg?v=1547583682&w=740&h=416",
             price : "$56"
+        }, {
+            id : 2,
+            name : "BeNTLY MEN",
+            imageUrl : "https://images-eu.ssl-images-amazon.com/images/I/415miCcMHzL._AC_UL160_.jpg",
+            price : "$126"
+        },{
+            id : 3,
+            name : "AFROJACK MEN'S AIR",
+            imageUrl : "https://images-eu.ssl-images-amazon.com/images/I/71gTE3UU3iL._AC_UL160_.jpg",
+            price : "$420"
+        },{
+            id : 4,
+            name : "AFROJACK MEN'S CRYPTO",
+            imageUrl : "https://images-na.ssl-images-amazon.com/images/I/41Zdk1zafJL.jpg",
+            price : "$176"
         }
     ]
 }
