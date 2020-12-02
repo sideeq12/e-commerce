4e3813404dc5380dc8a8509404e6aff214195321
sideeq12 <abdwaheed2018@gmail.com>
2020-12-02 05:26:56 -0800

    checking

--- src/components/preview-collection/InnerCollection.js
+++ src/components/preview-collection/InnerCollection.js
@@ -1,14 +1,14 @@
 import * as React from "react";
 
-function InnerCollection({ id, name, imageUrl }){
+function InnerCollection({  name, imageUrl, price }){
     return(
-        <div>
-        <div className="innerCollection" style={{
-            backgroundImage : imageUrl
-        }} /> 
-        <div>
-        <h4>{id}</h4>
-        <h6>{name}</h6>
+        <div className="innerCollection">
+        <div  style={{
+            backgroundImage : `url(${imageUrl})`
+        }} className="CollDesign" /> 
+        <div className="collection-footer">
+        <span>{name}</span> 
+        <div>{price}</div>
         </div>
         </div>
     )
--- src/components/preview-collection/previewCollection.js
+++ src/components/preview-collection/previewCollection.js
@@ -6,7 +6,7 @@ function Preview({title, content}){
         <div className="PreviewAll">
             <h2>{title}</h2>
             <div className="Preview">
-            {content.map((text) =>  <Inner key={text.id} name={text.name} imageUrl={text.imageUrl}  />
+            {content.map((text) =>  <Inner key={text.id} name={text.name} imageUrl={text.imageUrl} price={text.price}  />
             )}
             </div>
         </div>
